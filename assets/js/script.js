74de0a4eb60120af1b5a13ade3565ca144357ef1
EdivanAzevedo <66445757+EdivanAzevedo@users.noreply.github.com>
2023-06-06 09:58:12 -0300

    Realizado algumas correções de bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,19 @@ let binInp = document.getElementById("bin");
 let error = document.getElementById("error");
 
 decInp.addEventListener("input", () => {
-    let valorDec = parseInt(decInp.value);
-    binInp.value = valorDec.toString(2);
+    let decValue = parseInt(decInp.value);
+    binInp.value = decValue.toString(2);
 });
 
 binInp.addEventListener("input", () => {
-    let valorBin = binInp.value;
-
-    if (binValidador(binValue)) {
-        decInp.value = parseInt(valorBin, 2);
+    let binValue = binInp.value;
+    if (validadorBin(binValue)) {
+        decInp.value = parseInt(binValue, 2);
         error.textContent = "";
     } else {
         error.textContent = "Por favor, insira um número binário válido";
     }
-
-    function binValidador(num) {
+    function validadorBin(num) {
         for (let i = 0; i < num.length; i++) {
             if (num[i] != "0" && num[i] != "1") {
                 return false;

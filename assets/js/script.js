0609a3d0cba9432282efeeef607dd7a7fd21a480
EdivanAzevedo <66445757+EdivanAzevedo@users.noreply.github.com>
2023-06-06 11:42:13 -0300

    validador para decimal aceitar numeros positivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,20 @@ let error = document.getElementById("error");
 
 decInp.addEventListener("input", () => {
     let decValue = parseInt(decInp.value);
-    binInp.value = decValue.toString(2);
+    if (decValue < 0) {
+        error.textContent = "Por favor, insira um número positivo";
+        decInp.value = "";
+        return;
+    } else {
+        error.textContent = "";
+        binInp.value = decValue.toString(2);
+    }
 });
 
 binInp.addEventListener("input", () => {
     let binValue = binInp.value;
     if (validadorBin(binValue)) {
         decInp.value = parseInt(binValue, 2);
-        error.textContent = "";
     } else {
         error.textContent = "Por favor, insira um número binário válido";
     }
